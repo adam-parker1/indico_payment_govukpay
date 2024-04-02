6665ad744991d5126e2b7414a6e977f6131794bd
Adam Parker <adam.parker@ukaea.uk>
2024-04-02 19:37:11 +0100

    Initial commit

---
+++ indico_payment_govukpay/blueprint.py
@@ -0,0 +1,23 @@
+# This file is part of the Indico plugins.
+# Copyright (C) 2017 - 2024 Max Fischer, Martin Claus, CERN
+#
+# The Indico plugins are free software; you can redistribute
+# them and/or modify them under the terms of the MIT License;
+# see the LICENSE file for more details.
+
+from indico.core.plugins import IndicoPluginBlueprint
+
+# from indico_payment_sixpay.controllers import (RHInitSixpayPayment, SixpayNotificationHandler, UserCancelHandler,
+                                               # UserFailureHandler, UserSuccessHandler)
+from indico_payment_govukpay.controllers import RHInitGovukpayPayment
+
+blueprint = IndicoPluginBlueprint(
+    'payment_govukpay', __name__,
+    url_prefix='/event/<int:event_id>/registrations/<int:reg_form_id>/payment/govukpay'
+)
+
+blueprint.add_url_rule('/init', 'init', RHInitGovukpayPayment, methods=('GET', 'POST'))
+# blueprint.add_url_rule('/failure', 'failure', UserCancelHandler, methods=('GET', 'POST'))
+# blueprint.add_url_rule('/cancel', 'cancel', UserFailureHandler, methods=('GET', 'POST'))
+# blueprint.add_url_rule('/success', 'success', UserSuccessHandler, methods=('GET', 'POST'))
+# blueprint.add_url_rule('/notify', 'notify', SixpayNotificationHandler, methods=('Get', 'POST'))
